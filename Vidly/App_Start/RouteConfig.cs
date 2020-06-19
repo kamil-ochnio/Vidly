74777bb212c0fc2723601011a838b6824091f32d
kochnio <akamil47@gmail.com>
2020-06-19 10:47:35 +0200

    add migration

--- Vidly/App_Start/RouteConfig.cs
+++ Vidly/App_Start/RouteConfig.cs
@@ -22,7 +22,7 @@ namespace Vidly
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = " ", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
